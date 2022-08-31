8e9fb1e4cc389b01b593a750dcfb9e431b676b81
Oliver Granudden <oliver.granudden@elev.ga.ntig.se>
2022-08-31 14:22:28 +0200

    finished lucktext

---
+++ lucktext/Program.cs
@@ -0,0 +1,15 @@
+﻿string name;
+string klass;
+string feelingToday;
+
+Console.WriteLine("Hello, what is your name?");
+name = Console.ReadLine();
+
+Console.WriteLine("Which class do you go in?");
+klass = Console.ReadLine();
+
+Console.WriteLine("How are you feeling today?");
+feelingToday = Console.ReadLine();
+
+Console.WriteLine($"{name} from class {klass} is feeling {feelingToday} today.");
+Console.ReadLine();
